516cbc539c9bba92644a8c1608285882cb755ea0
ismasan <ismaelct@gmail.com>
2009-09-30 02:46:53 -0700

    Corrected code comments for MiniFlickr::Simple

--- lib/mini_flickr/simple.rb
+++ lib/mini_flickr/simple.rb
@@ -2,12 +2,12 @@ module MiniFlickr
   # This class is meant to load api_key and user_id per instance instead of at class-level
   #
   # Example:
-  # flickr = MiniFlickr.new(api_key, user_id)
+  # flickr = MiniFlickr::Simple.new(api_key, user_id)
   # flickr.photos
   #
   # Or as a value object in ActiveRecord
   #
-  # class Flickr < MiniFlickr; end
+  # class Flickr < MiniFlickr::Simple; end
   #
   # composed_of :flickr, :mappings => %w(api_key user_id)
   #
